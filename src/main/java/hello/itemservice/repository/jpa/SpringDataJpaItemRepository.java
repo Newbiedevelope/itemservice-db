4bee6c4c4c994f6bc044c8e6c740df1dd3a57167
Newbiedevelope <rbtjr20313@gmail.com>
2023-03-15 11:38:18 +0900

    sixth commit

---
+++ src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -0,0 +1,22 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNameLike(String itemName);
+
+    List<Item> findByPriceLessThanEqual(Integer price);
+
+    //쿼리 메서드, 메서드 이름이 지나치게 길다
+    List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName, Integer price);
+
+    //쿼리 직접 실행, 후에 QueryDsl을 이용하면 편리해짐
+    @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+}
